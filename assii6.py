be5df02866151b97bf9a35675c3152b2a7413339
Priyankashirsat123 <70810147+Priyankashirsat123@users.noreply.github.com>
2020-09-13 22:32:35 +0530

    Add files via upload

--- assii6.py
+++ assii6.py
@@ -1,3 +1,43 @@
+#assisgnment 6.1
+class Bank_account:
+    def __init__(self):
+        self.balance = 0
+        print('HELLO!!!! Welcome To The Deposit & Withdrawal Machine')
+
+    def Deposit(self):
+        amount = float(input("Enter amount to be deposited:"))
+        self.balance += amount
+        print("\n Amount Deposited: ",amount)
+
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn:"))
+        if self.balance >= amount:
+            self.balance -= amount
+            print("\n You withdrew: ",amount)
+        else:
+            print("\n Insufficient Balance ")
+    def display(self):
+        print("\n Net Available Balance = ",self.balance)
+s = Bank_account()
+s.Deposit()
+s.withdraw()
+s.display()
+
+"""
+OUTPUT
+
+HELLO!!!! Welcome To The Deposit & Withdrawal Machine
+Enter amount to be deposited:5000
+
+ Amount Deposited:  5000.0
+Enter amount to be Withdrawn:1000
+
+ You withdrew:  1000.0
+
+ Net Available Balance =  4000.0
+ 
+ """
+
 #assisgnment 6.2
 
 import math
